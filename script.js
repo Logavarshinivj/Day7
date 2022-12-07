c28b27787e82a11c309f0fdb60686af81b05fbec
Logavarshinivj <118879051+Logavarshinivj@users.noreply.github.com>
2022-12-07 22:00:49 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,60 +1,39 @@
 
-// var request= new XMLHttpRequest()
-// request.open("GET","https://restcountries.com/v3.1/all",true)
-// request.send()
-// request.onload=function()
-// {
-//     var data= request.response
-//     var result=JSON.parse(data)
-//     var res1=result.filter((ele)=>ele.population<200000)
-//     var res2=res1.map((ele)=>ele.name.common)
-//     var res3=res2.map((ele)=>ele.population)
-//     console.log(res2,res3)
-// }
-
-
-// var request=new XMLHttpRequest()
-// request.open("GET","https://restcountries.com/v3.1/all",true)
-// request.send()
-// request.onload=function()
-// {
-//     var data=request.response
-//     var result=JSON.parse(data)
-//     var res1=result.forEach(ele=>{
-//         console.log(ele.name.common,ele.flag,ele.capital)
-//     })
-        
-   
-// }
-
-// var request = new XMLHttpRequest();
-// request.open("GET","https://restcountries.com/v3.1/all",true)
-// request.send()
-//  request.onload=function(){
-//     var data = request.response
-//     var result = JSON.parse(data)
-//     var res = result.reduce((acc,cv)=>acc+cv.population,0)
-//     console.log(res)
-//     }
-  
-// var request=new XMLHttpRequest();
-// request.open("GET","https://restcountries.com/v3.1/all",true)
-// request.send()
-//   request.onload=function(){
-//     var data=request.response
-//     var result=JSON.parse(data)
-//     var res1= result.filter((ele)=>ele.region=="Asia")
-//     var res2= res1.map((ele)=>ele.name.common)
-//     console.log(res2)
-//   }
-
-var request=new XMLHttpRequest()
-request.open("GET","https://restcountries.com/v3.1/all",true)
+var request= new XMLHttpRequest();
+request.open("GET","https://restcountries.com/v3.1/all")
 request.send()
-  request.onload=function(){
+request.onload=function()
+{
     var data=request.response
     var result=JSON.parse(data)
-    var res1=result.filter((ele)=>ele.currencies=="USD")
-    var res2=res1.map((ele)=>ele.name.common)
-    console.log(res2)
-  }
+    for (var i of result){
+    console.log(i.flag);
+    }
+}
+// }
+// {
+//     var data=request.response
+//     var result=JSON.parse(data)
+//     for (var i of result){
+//         console.log(i.name.common,i.region,i.subregion,i.population)
+//     }
+// }
+
+//compare JSON o
+const _ = require('lodash'); 
+var obj1={
+    "name":"person1",
+    "age":21,
+    "sex":"male"
+}
+var obj2={
+    "age":21,
+    "sex":"male",
+    "name":"person1",
+}
+if(_.isEqual(obj1, obj2)){
+    console.log("we are equal");
+}
+else{
+    console.log("we are not equal");
+}
